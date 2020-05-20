0b29849a3380fb66bc6050834fa803e2434414b3
starchoi <honey333888@naver.com>
2020-05-21 03:12:56 +0900

    메인 화면 구성 및 공지사항 디자인

---
+++ app/src/main/java/com/example/bookmanage/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.bookmanage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noticeListView = (ListView) findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        noticeList.add(new Notice("공지사항입니다.", "최별 정주희 김예지", "2020-05-21"));
+        adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
+
+        final Button bookButton = (Button) findViewById(R.id.bookButton);
+        final Button statisticsButton = (Button) findViewById(R.id.statisticsButton);
+        final Button accountButton = (Button) findViewById(R.id.accountButton);
+        final LinearLayout notice = (LinearLayout) findViewById(R.id.notice);
+
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(v.GONE);
+                bookButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                accountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new BookFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(v.GONE);
+                bookButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                accountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new StatisticsFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notice.setVisibility(v.GONE);
+                bookButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                statisticsButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                accountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, new AccountFragment());
+                fragmentTransaction.commit();
+            }
+        });
+    }
+}
