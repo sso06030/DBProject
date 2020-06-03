0bfa543726227522e6af55d7138578d81c0c53ca
juhee <jieboong82@khu.ac.kr>
2020-06-04 00:00:10 +0900

    BookListAdapter 추가

---
+++ app/src/main/java/com/example/bookmanage/BookListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.bookmanage;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class BookListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Book> bookList;
+
+    public BookListAdapter(Context context, List<Book> bookList) {
+        this.context = context;
+        this.bookList = bookList;
+    }
+
+    @Override
+    public int getCount() {
+        return bookList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {return bookList.get(i);}
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        View v = View.inflate(context, R.layout.book, null);
+
+        TextView book_Id = (TextView) v.findViewById(R.id.bookID);
+        TextView ISBN = (TextView) v.findViewById(R.id.bookISBN);
+        TextView Title = (TextView)v.findViewById(R.id.bookTitle);
+        TextView bookAuthor = (TextView)v.findViewById(R.id.bookAuthor);
+        TextView Genre = (TextView) v.findViewById(R.id.bookGenre);
+        TextView publisher = (TextView) v.findViewById(R.id.bookPublisher);
+        TextView price = (TextView) v.findViewById(R.id.bookPrice);
+
+        book_Id.setText(String.valueOf(bookList.get(i).getBook_id()));
+        ISBN.setText(bookList.get(i).getISBN());
+        Title.setText(bookList.get(i).getTitle());
+        bookAuthor.setText(bookList.get(i).getAuthor());
+        Genre.setText(bookList.get(i).getGenre());
+        publisher.setText(bookList.get(i).getPublisher());
+        price.setText(String.valueOf(bookList.get(i).getPrice()));
+
+        v.setTag(bookList.get(i).getTitle());
+        return v;
+    }
+}
