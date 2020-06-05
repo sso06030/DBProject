e261b380b074c836f61fbd49d8852a992f02920d
Jung Ju Hee <jieboong82@daum.net>
2020-06-05 21:58:21 +0900

    대여 버튼 기능 구현

--- app/src/main/java/com/example/bookmanage/BookListAdapter.java
+++ app/src/main/java/com/example/bookmanage/BookListAdapter.java
@@ -1,11 +1,20 @@
 package com.example.bookmanage;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class BookListAdapter extends BaseAdapter {
@@ -52,6 +61,48 @@ public class BookListAdapter extends BaseAdapter {
         price.setText(String.valueOf(bookList.get(i).getPrice()));
 
         v.setTag(bookList.get(i).getTitle());
+
+        final DBHelper dbHelper = new DBHelper(context);
+        final SQLiteDatabase database = dbHelper.getReadableDatabase();
+        final int bookId = bookList.get(i).getBook_id();
+        String sql = "SELECT rent_num FROM RENT WHERE book_id = " +bookId;
+        Cursor cursor = database.rawQuery(sql,null);
+
+        Button rentButton = (Button) v.findViewById(R.id.rentButton);
+        if (cursor.getCount() != 0) {
+            rentButton.setBackgroundColor(Color.GRAY);
+            rentButton.setText("대여중");
+            rentButton.setEnabled(false);
+        }
+        else{
+            rentButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) { SQLiteDatabase db = dbHelper.getWritableDatabase();
+                String userID = MainActivity.UserID;
+
+                Long now = System.currentTimeMillis();
+                Date date = new Date(now);
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(new Date());
+                cal.add(Calendar.DATE,14);
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+                int RentNum = (int)date.getTime()+bookId;
+
+                db.execSQL("INSERT INTO BORROW (RENT_NUM,RENT_DATE,RETURN_DATE)" + "VALUES(?,?,?)",new Object[]{RentNum,simpleDateFormat.format(date),simpleDateFormat.format(cal.getTime())});
+                db.execSQL("INSERT INTO RENT (book_id,user_id,rent_num) VALUES(?,?,?)",new Object[]{RentNum,userID,RentNum});
+
+                Toast t = Toast.makeText(context,"이건됨",Toast.LENGTH_SHORT);
+                t.show();
+//                AlertDialog dialog;
+//                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//                dialog = builder.setMessage("대출되었습니다.")
+//                        .setPositiveButton("확인",null)
+//                        .create();
+//                dialog.show();
+                }
+            });
+        }
         return v;
     }
 }
